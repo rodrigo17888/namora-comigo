a3176103a08969080bac9f78f65b8584ea2c281b
rodrigo17888 <rodrigosilvalopes16@gmail.com>
2024-01-17 22:22:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function fuja() {
+  let btnNao = document.getElementById("nao");
+
+  let larguraJanela = window.innerWidth;
+  let alturaJanela = window.innerHeight;
+
+  let maxX = larguraJanela - btnNao.offsetWidth;
+  let maxY = alturaJanela - btnNao.offsetHeight;
+
+  let aleatorioX = Math.floor(Math.random() * maxX);
+  let aleatorioY = Math.floor(Math.random() * maxY);
+
+  btnNao.style.left = aleatorioX + "px";
+  btnNao.style.top = aleatorioY + "px";
+}
+
+function aceito() {
+  alert("Parabén você fez uma ótima escolha hehehe !!!");
+}
