bd742e88a09151e961d507d7e1547d562ef41982
rodrigo17888 <rodrigosilvalopes16@gmail.com>
2024-01-18 13:20:36 -0300

    Adicionei um video ao apertar o sim, mudei a largura e o tamanho para telas menores, adicionei novas funcionalidades ao apertar o 'SIM' por exemplo os botões sairam e fica só o video sedutor com uma frase abaixo

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Encontrar o elemento de imagem pelo ID
+  var imagemElement = document.getElementById("image");
+
+  // Encontrar o botão pelo ID
+  var btnAceito = document.getElementById("sim");
+
+  const h2 = document.querySelector(".pergunta");
+  const btn1 = document.querySelector("#sim");
+  const btn2 = document.querySelector("#nao");
+  btnAceito.addEventListener("click", function () {
+    h2.innerHTML = "Parabéns, você fez uma ótima escolha !";
+    h2.style.marginTop = "70px";
+    h2.style.fontSize = "16px";
+    btn1.style.display = "none";
+    btn2.style.display = "none";
+  });
+
+  // Função para trocar a imagem pelo vídeo
+  function trocarParaVideo() {
+    // Criar um elemento de vídeo
+    var videoElement = document.createElement("video");
+
+    // Configurar atributos do vídeo (src, controls, etc.)
+    videoElement.src = "videos/olhar-sedutor.mp4";
+    videoElement.autoplay = true;
+    videoElement.loop = true;
+    videoElement.style.width = "300px";
+
+    // Substituir a imagem pelo vídeo
+    imagemElement.parentNode.replaceChild(videoElement, imagemElement);
+
+    // Remover o ouvinte de clique após a troca
+    btnAceito.removeEventListener("click", trocarParaVideo);
+  }
+
+  // Adicionar um ouvinte de clique ao botão
+  btnAceito.addEventListener("click", trocarParaVideo);
+});
+
 function fuja() {
   let btnNao = document.getElementById("nao");
 
@@ -12,8 +52,4 @@ function fuja() {
 
   btnNao.style.left = aleatorioX + "px";
   btnNao.style.top = aleatorioY + "px";
-}
-
-function aceito() {
-  alert("Parabéns, você fez uma ótima escolha hehehe !!!");
 }
